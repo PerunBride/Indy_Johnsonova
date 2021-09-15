6e4fb8cdc5c67d40d1ab45acdc69ed8e2fc1f198
Schottova <jana.schottova@t-systems.com>
2021-09-15 17:04:58 +0200

    python skrypty uploadovane

---
+++ commands.py
@@ -0,0 +1,238 @@
+import random
+
+import features
+import states
+import usages
+from helper import find_item
+
+
+def inventory(name: str, context: dict) -> None:
+    if len(context['inventory']) == 0:
+        print('Batoh je prázdny.')
+    else:
+        print('V batohu máš:')
+        for item in context['inventory']:
+            print(f'\t* {item["name"]}')
+
+
+
+def look_around(name: str, context: dict) -> None:
+    room = context['room']
+    print(f'Nachádzaš sa v miestnosti {room["name"]}.')
+    print(f'{room["description"]}')
+
+    if len(room['items']) == 0:
+        print('Nevidíš tu nič zaujímavé.')
+    else:
+        print(f'Vidíš:')
+        for item in room['items']:
+            print(f'\t* {item["name"]}')
+
+
+
+    # tuto vypisat vychody z miestnosti
+    # alebo: Z miestnosti nevedú žiadne východy. v pripade, ze ziadne vychody nie su
+
+
+def drop(name: str, context: dict) -> None:
+    if name == '':
+        print('Neviem, aký predmet chceš položiť.')
+    else:
+        for item in context['inventory']:
+            if item['name'] == name:
+                # vybrat ho z inventaru
+                context['inventory'].remove(item)
+
+                # polozit ho do miestnosti
+                context['room']['items'].append(item)
+
+                print(f'Predmet {name} si vyložil do miestnosti.')
+                break
+        else:
+            print('Taký predmet u seba nemáš.')
+
+
+def take(name: str, context: dict) -> None:
+    """
+    Represents the TAKE command.
+    :param name: the name of item do describe
+    :param room: the room object, where the player is currently in
+    :param inventory: the player's inventory
+    """
+
+    room = context['room']
+    inventory = context['inventory']
+
+    if name == '':
+        print('Neviem, aký predmet chceš vziať.')
+    else:
+        for item in room['items']:
+            if item['name'] == name:
+                # overit, ci je batoh plny
+                # todo: zmeni konstantu na premennu
+                if len(inventory) >= 2:
+                    print('Batoh je plný.')
+
+                # overit, ci sa da zobrat
+                elif features.MOVABLE in item['features']:
+                    # vybrat ho z roomu
+                    room['items'].remove(item)
+
+                    # vlozit do batohu
+                    inventory.append(item)
+
+                    print(f'Predmet {name} si si vložil do batohu.')
+                else:
+                    print('Tento predmet sa nedá zobrať.')
+
+                break
+        else:
+            print('Taký predmet tu nikde nevidím.')
+
+
+def examine(name: str, context: dict) -> None:
+    """
+    Represents the examine command.
+    :param name: the name of item do describe
+    :param room: the room object, where the player is currently in
+    :param inventory: the player's inventory
+    """
+
+    room = context['room']
+    inventory = context['inventory']
+
+    if len(name) == 0:
+        print('Neviem, aký predmet chceš preskúmať.')
+    else:
+        for item in room['items'] + inventory:
+            if item['name'] == name:
+                print(item['description'])
+                break
+        else:
+            print('Taký predmet tu nigde nevidím.')
+
+
+def use(name: str, context: dict) -> None:
+    """
+    Represents the examine command.
+    :param name: the name of item do describe
+    :param context: the game context
+    """
+
+    room = context['room']
+    inventory = context['inventory']
+
+    # nezadal meno predmetu
+    if len(name) == 0:
+        print('Neviem, aký predmet chceš použiť.')
+        return
+
+    # najdem predmet v miestnosti alebo inventari
+    item = find_item(name, room['items'] + inventory)
+    if item is None:
+        print('Taký predmet tu nikde nevidím.')
+        return
+
+    # overim, ci je predmet pouzitelny
+    if features.USABLE not in item['features']:
+        print('Tento predmet sa nedá použiť.')
+        return
+
+    # pouzitie predmetov
+    if name == 'ucebnica jazyka python':
+        usages.use_textbook(item, context)
+
+    elif name == 'kanister':
+        usages.use_can(item, context)
+
+    elif name == 'zapalky':
+        usages.use_matches(item, context)
+
+    elif name == 'hasiaci pristroj':
+        usages.use_fire_extinguisher(item, context)
+
+    else:
+        print(f'Snažím sa použiť predmet {name}')
+
+
+def about(name: str, context: dict) -> None:
+    print('Hru spáchal v (c) 2021 mirek.')
+    print('Ďalšie dobrodužstvo Indiana Jonesa. Tentokrát sa pokúsi o únik zo skladu Košického Technického múzea.')
+
+
+def quit_game(name: str, context: dict) -> None:
+    print('ta koncime')
+    context['state'] = states.QUIT
+
+
+def show_commands(name: str, context: dict) -> None:
+    print('Zoznam príkazov hry:')
+
+    for command in context['commands']:
+        print(f'\t* {command["name"]} - {command["description"]}')
+
+
+commands = [
+    {
+        'name': 'preskumaj',
+        'aliases': ('examine',),
+        'exec': examine,
+        'description': 'zobrazí informácie o zvolenom predmete'
+    },
+
+    {
+        'name': 'poloz',
+        'aliases': ('drop',),
+        'exec': drop,
+        'description': 'vyberie predmet z batohu a položí ho do miestnosti'
+    },
+
+    {
+        'name': 'koniec',
+        'aliases': ('quit', 'bye', 'q', 'ukoncit'),
+        'description': 'ukončí rozohratú hru',
+        'exec': quit_game
+    },
+
+    {
+        'name': 'o hre',
+        'aliases': ('about',),
+        'description': 'zobrazí informácie o hre',
+        'exec': about
+    },
+
+    {
+        'name': 'rozhliadni sa',
+        'aliases': ('look around',),
+        'description': 'zobrazí obsah miestnosti',
+        'exec': look_around
+    },
+
+    {
+        'name': 'inventar',
+        'aliases': ('inventory', 'i'),
+        'description': 'zobrazí obsah batohu',
+        'exec': inventory
+    },
+
+    {
+        'name': 'vezmi',
+        'aliases': ('take',),
+        'description': 'vezme predmet z miestnosti a vloží ho do batohu',
+        'exec': take
+    },
+
+    {
+        'name': 'prikazy',
+        'aliases': ('commands', 'help', 'pomoc'),
+        'description': 'zobrazí zoznam príkazov dostupných v hre',
+        'exec': None  # commands
+    },
+
+    {
+        'name': 'pouzi',
+        'aliases': ('pouzit', 'use'),
+        'description': 'použije zvolený predmet',
+        'exec': use
+    }
+]
---
+++ main.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import features
+import states
+import commands
+
+
+def parse(line: str, commands: list) -> tuple:
+    for command in commands:
+        for alias in command['aliases'] + (command['name'],):
+            if line.startswith(alias):
+                param = line.split(alias)[1].strip()
+                return (command, param)
+
+    return (None, None)
+
+
+def main():
+    context = {
+        'commands': None,
+        'inventory': None,
+        'state': states.PLAYING,
+        'room': None,
+        'world': None
+    }
+
+    context['commands'] = commands.commands
+    world = [
+        {
+            'name': 'dungeon',
+            'description': 'Nachádzaš sa v tmavej miestnosti. Každé okno je zvonku zabarikádované a do miestnosti preniká len '
+                           'úzky prameň svetla. Masívne drevené dvere sú jediným východom z miestnosti.',
+            'items': [
+                {
+                    'name': 'kanister',
+                    'description': 'Kanister plný benzínu.',
+                    'features': [features.MOVABLE, features.USABLE]
+                },
+
+                {
+                    'name': 'hasiaci pristroj',
+                    'description': 'Ručný hasiaci prístroj plný. Značka - červený.',
+                    'features': [features.MOVABLE, features.USABLE]
+                },
+
+                {
+                    'name': 'zapalky',
+                    'description': 'Krabička zápaliek vyrobená ešte v Československu. Kvalitka.',
+                    'features': [features.MOVABLE, features.USABLE],
+                    'total': 3,
+                },
+
+                {
+                    'name': 'dvere',
+                    'description': 'Veľké masívne drevené dvere. Zamknuté.',
+                    'features': [],
+                    'state': 'zamknute'
+                }
+            ],
+            'exits': {
+                'west': 'garden',
+                'east': None,
+                'south': None,
+                'north': None
+            }
+        },
+
+        {
+            'name': 'garden',
+            'description': 'Pomerne zarastené hriadky niečoho, čo by sa dalo voľne nazvať záhradkou. Darí sa tu skorocelu a inej burine.',
+            'items': [],
+            'exits': {
+                'east': 'dungeon',
+                'west': None,
+                'north': None,
+                'south': None
+            }
+        }
+    ]
+
+    context['room'] = world[0]
+
+    context['inventory'] = [
+        {
+            'name': 'ucebnica jazyka python',
+            'description': 'Mocná učebnica jazyka Python od známeho Pytonistu Jana.',
+            'features': [features.MOVABLE, features.USABLE]
+        }
+    ]
+
+    commands.look_around(None, context)
+
+    # game loop
+    while context['state'] == states.PLAYING:
+        line = input('> ').strip().lower()
+
+        # parse input line
+        command, param = parse(line, context['commands'])
+        if command is not None:
+            command['exec'](param, context)
+        else:
+            print('Taký príkaz nepoznám.')
+
+    print('...koniec...')
+
+
+if __name__ == '__main__':
+    main()
